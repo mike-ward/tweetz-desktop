e874f10c9926a163d42f78e962a9c0bba31b9f1c
Mike Ward <mike@wardfam.org>
2012-11-10 03:22:04 +0000

    Trying mstest to see if it runs with TFS
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C17

---
+++ tweetz5/tweetz5UnitTests/TimelineControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using tweetz5;
+using tweetz5.Model;
+using tweetz5.Utilities.System;
+
+namespace tweetz5UnitTests
+{
+    [TestClass]
+    public class TimelineControllerTests
+    {
+        [TestMethod]
+        public void ConstructingControllerStartsTimelines()
+        {
+            var checkTimelines = new Mock<ITimer>();
+            var updateTimelines = new Mock<ITimer>();
+            var queue = new Queue<ITimer>();
+            queue.Enqueue(checkTimelines.Object);
+            queue.Enqueue(updateTimelines.Object);
+            SysTimer.Override = queue.Dequeue;
+
+            var timelines = new Mock<ITimelines>();
+            var controller = new TimelineController(timelines.Object);
+            controller.StartTimelines();
+            checkTimelines.Raise(c => c.Elapsed += null, EventArgs.Empty);
+            updateTimelines.Raise(u => u.Elapsed += null, EventArgs.Empty);
+
+            checkTimelines.VerifySet(c => c.Interval = 100);
+            checkTimelines.VerifySet(c => c.Interval = 60000);
+            checkTimelines.Verify(c => c.Start());
+            timelines.Verify(t => t.HomeTimeline());
+            timelines.Verify(t => t.MentionsTimeline());
+
+            updateTimelines.VerifySet(u => u.Interval = 30000);
+            updateTimelines.Verify(u => u.Start());
+            timelines.Verify(t => t.UpdateTimeStamps());
+            Assert.IsNotNull(controller);
+        }
+    }
+}
