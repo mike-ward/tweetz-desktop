103f47e772e14829117f9a891e2aec68c66b6c0b
Mike Ward <mike@wardfam.org>
2012-12-16 14:34:22 +0000

    Add resizing via thumb control
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C72

--- tweetz5/tweetz5/MainWindow.xaml.cs
+++ tweetz5/tweetz5/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿// Copyright (c) 2012 Blue Onion Software. All rights reserved.
 
+using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
 namespace tweetz5
 {
     public partial class MainWindow
@@ -12,6 +16,16 @@ namespace tweetz5
         private void DragMoveWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             DragMove();
+        }
+
+        private void ThumbOnDragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Height = Math.Max(Height + e.VerticalChange, MinHeight);
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _timeline.Height = e.NewSize.Height - 19;
         }
     }
 }
