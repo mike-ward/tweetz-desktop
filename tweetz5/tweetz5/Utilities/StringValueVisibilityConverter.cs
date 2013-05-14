3aa14310a418a8ca53b8ee834d1fb584c1720406
Mike Ward <mike@wardfam.org>
2013-05-14 12:11:11 +0000

    Some how, TFS missed moving this file
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C266

---
+++ tweetz5/tweetz5/Utilities/StringValueVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2012 Blue Onion Software - All rights reserved
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace tweetz5.Utilities
+{
+    [ValueConversion(typeof (string), typeof (Visibility))]
+    public class StringValueVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
