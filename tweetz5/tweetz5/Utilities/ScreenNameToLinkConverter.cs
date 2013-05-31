7e0722071c94ee9f9a50fb5be8a48a41e9acbe59
Mike Ward <mike@wardfam.org>
2013-05-31 22:26:55 +0000

    Add link to screen name in about user popup
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C371

---
+++ tweetz5/tweetz5/Utilities/ScreenNameToLinkConverter.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2013 Blue Onion Software - All rights reserved
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace tweetz5.Utilities
+{
+    [ValueConversion(typeof (string), typeof (string))]
+    internal class ScreenNameToLinkConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format("https://twitter.com/{0}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
