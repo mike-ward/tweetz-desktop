9cd065111fcb4c930852f096b788fedebd115786
Mike Ward <mike@wardfam.org>
2013-07-03 22:43:12 +0000

    Fix mytweet color Fix image locking on drag and drop
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C700

---
+++ tweetz5/tweetz5/Utilities/UriToCachedImageConverter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2013 Blue Onion Software - All rights reserved
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace tweetz5.Utilities
+{
+    public class UriToCachedImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DependencyProperty.UnsetValue;
+
+            if (!string.IsNullOrEmpty(value.ToString()))
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(value.ToString());
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                return bitmap;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Two way conversion is not supported.");
+        }
+    }
+}
