cf8bcc4d0fd3a21b140363301a61ea769d9880c0
Mike Ward <mike@wardfam.org>
2013-09-22 15:12:34 +0000

    Add popup escape as a behavior
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C804

--- tweetz5/tweetz5/Controls/ShortcutHelp.xaml.cs
+++ tweetz5/tweetz5/Controls/ShortcutHelp.xaml.cs
@@ -6,20 +6,9 @@ namespace tweetz5.Controls
 {
     public partial class ShortcutHelp
     {
-        private IInputElement _element;
-
         public ShortcutHelp()
         {
             InitializeComponent();
-            Loaded += (sender, args) => Child.IsVisibleChanged += (o, eventArgs) =>
-            {
-                if (Child.IsVisible)
-                {
-                    _element = Keyboard.FocusedElement;
-                    Keyboard.Focus(Child);
-                }
-            };
-            Closed += (sender, args) => Keyboard.Focus(_element);
         }
 
         public struct KeyboardShortcut
@@ -30,7 +19,7 @@ namespace tweetz5.Controls
 
         private readonly KeyboardShortcut[] _keyboardShortcuts = new []
         {
-            new KeyboardShortcut {Shortcut = "Ctrl + X", Description = "Close"},
+            new KeyboardShortcut {Shortcut = "Ctrl+X", Description = "Close"},
             new KeyboardShortcut {Shortcut = "J", Description = "Next tweet"},
             new KeyboardShortcut {Shortcut = "K", Description = "Previous tweet"},
             new KeyboardShortcut {Shortcut = "R", Description = "Reply"},
@@ -38,8 +27,8 @@ namespace tweetz5.Controls
             new KeyboardShortcut {Shortcut = "F", Description = "Favorite"},
             new KeyboardShortcut {Shortcut = "N", Description = "New tweet"},
             new KeyboardShortcut {Shortcut = "/", Description = "Search"},
-            new KeyboardShortcut {Shortcut = "Ctrl + Home", Description = "Go to top"},
-            new KeyboardShortcut {Shortcut = "Ctrl + End", Description = "Go to bottom"},
+            new KeyboardShortcut {Shortcut = "Ctrl+Home", Description = "Go to top"},
+            new KeyboardShortcut {Shortcut = "Ctrl+End", Description = "Go to bottom"}
         };
 
         public IEnumerable<KeyboardShortcut> KeyboardShortcuts
