92dcc5f9847bab1aa85f08db07406406c5a9e146
Mike Ward <mike@wardfam.org>
2013-12-04 14:43:53 +0000

    Fix profile to use localized strings
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C1070

--- tweetz5/tweetz5/Properties/AssemblyInfo.cs
+++ tweetz5/tweetz5/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None,ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("0.7.2")]
-[assembly: AssemblyFileVersion("0.7.2")]
+[assembly: AssemblyVersion("0.7.3")]
+[assembly: AssemblyFileVersion("0.7.3")]
 [assembly: Guid("B21EBDFF-5222-451A-AB7D-F07EF6DF2813")]
