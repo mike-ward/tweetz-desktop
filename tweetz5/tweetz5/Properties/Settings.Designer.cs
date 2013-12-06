baaef035e3b79865a9c913e5dc1e851b42966ce4
Mike Ward <mike@wardfam.org>
2013-12-06 21:08:36 +0000

    Change default size to 500
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C1086

--- tweetz5/tweetz5/Properties/Settings.Designer.cs
+++ tweetz5/tweetz5/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace tweetz5.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -49,7 +49,7 @@ namespace tweetz5.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("700")]
+        [global::System.Configuration.DefaultSettingValueAttribute("500")]
         public double Height {
             get {
                 return ((double)(this["Height"]));
