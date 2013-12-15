d2ee5def11b7eac72a4da285e57463bfe7842158
Mike Ward <mike@wardfam.org>
2013-12-15 15:11:46 +0000

    Minor fix to German translation, bump verion 0.7.7
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C1153

--- tweetz5/Setup/tweetz5/Setup/Setup.iss
+++ tweetz5/Setup/tweetz5/Setup/Setup.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Tweetz Desktop"
-#define MyAppVersion "0.7.6"
+#define MyAppVersion "0.7.7"
 #define MyAppPublisher "Mike Ward"
 #define MyAppURL "http://mike-ward.net"
 #define MyAppExeName "tweetz5.exe"
--- tweetz5/tweetz5/Properties/AssemblyInfo.cs
+++ tweetz5/tweetz5/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) 2013 Blue Onion Software - All rights reserved
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -9,21 +7,13 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mike-Ward.Net")]
 [assembly: AssemblyProduct("Tweetz Desktop")]
-[assembly: AssemblyCopyright("Copyright ©  2013 Mike-Ward.Net, All rights reserved")]
+[assembly: AssemblyCopyright("Copyright ©  2013 Mike-Ward, All rights reserved")]
 [assembly: AssemblyTrademark("Tweetz is a trademark of Mike Ward")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None,ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("0.7.6")]
-[assembly: AssemblyFileVersion("0.7.6")]
+[assembly: AssemblyVersion("0.7.7")]
+[assembly: AssemblyFileVersion("0.7.7")]
 [assembly: Guid("B21EBDFF-5222-451A-AB7D-F07EF6DF2813")]
