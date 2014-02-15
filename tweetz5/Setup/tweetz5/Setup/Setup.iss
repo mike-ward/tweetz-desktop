4f917bd72e88048f7ef017088244af0df4c86f44
Mike Ward <mike@wardfam.org>
2014-02-15 16:30:26 +0000

    Bump version 0.8.3
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C1519

--- tweetz5/Setup/tweetz5/Setup/Setup.iss
+++ tweetz5/Setup/tweetz5/Setup/Setup.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Tweetz Desktop"
-#define MyAppVersion "0.8.2"
+#define MyAppVersion "0.8.3"
 #define MyAppPublisher "Mike Ward"
 #define MyAppURL "http://mike-ward.net"
 #define MyAppExeName "tweetz5.exe"
--- tweetz5/tweetz5/Properties/AssemblyInfo.cs
+++ tweetz5/tweetz5/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None,ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("0.8.2")]
-[assembly: AssemblyFileVersion("0.8.2")]
+[assembly: AssemblyVersion("0.8.3")]
+[assembly: AssemblyFileVersion("0.8.3")]
 [assembly: Guid("B21EBDFF-5222-451A-AB7D-F07EF6DF2813")]
