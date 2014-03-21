015cc55f355da3cf5a22775c9d25d42806315cd6
Mike Ward <mike@wardfam.org>
2014-03-21 20:40:10 +0000

    More resilient login
    git-tfs-id: [https://mikeward.visualstudio.com/DefaultCollection]$/tweetz5;C1815

--- tweetz5/tweetz5/Controls/Authenticate.xaml.cs
+++ tweetz5/tweetz5/Controls/Authenticate.xaml.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) 2013 Blue Onion Software - All rights reserved
-
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using tweetz5.Commands;
 using tweetz5.Model;
@@ -7,13 +7,24 @@ using Settings = tweetz5.Properties.Settings;
 
 namespace tweetz5.Controls
 {
-    public partial class Authenticate
+    public partial class Authenticate : INotifyPropertyChanged
     {
-        private Twitter.OAuthTokens Tokens { get; set; }
+        private Twitter.OAuthTokens _tokens;
 
         public Authenticate()
         {
             InitializeComponent();
+        }
+
+        public Twitter.OAuthTokens Tokens
+        {
+            get { return _tokens; }
+            set
+            {
+                if (_tokens == value) return;
+                _tokens = value;
+                OnPropertyChanged();
+            }
         }
 
         private async void GetPin_OnClick(object sender, RoutedEventArgs e)
@@ -27,12 +38,21 @@ namespace tweetz5.Controls
         {
             var tokens = await Twitter.GetAccessToken(Tokens.OAuthToken, Tokens.OAuthSecret, Pin.Text);
             Pin.Text = string.Empty;
+            if (tokens == null) return;
             Settings.Default.AccessToken = tokens.OAuthToken;
             Settings.Default.AccessTokenSecret = tokens.OAuthSecret;
             Settings.Default.UserId = tokens.UserId;
             Settings.Default.ScreenName = tokens.ScreenName;
             Settings.Default.Save();
             SignInCommand.Command.Execute(null, this);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
