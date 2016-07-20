150cea04efec1460c232bc7e1135410f180b362b
mike-ward <mike@mike-ward.net>
2016-07-20 18:36:08 -0400

    1.1.0

--- tweetz/tweetz5/Properties/AssemblyInfo.cs
+++ tweetz/tweetz5/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None,ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: Guid("B21EBDFF-5222-451A-AB7D-F07EF6DF2813")]
